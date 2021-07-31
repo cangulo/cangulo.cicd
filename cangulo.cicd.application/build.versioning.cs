db1b59d85794793ac3841626527d5c111ebe6be6
Carlos Miguel Angulo Mascarell <carlos.angulo.mascarell@outlook.com>
2021-08-01 01:49:21 +0200

    feat:wip

--- cangulo.cicd.application/build.versioning.cs
+++ cangulo.cicd.application/build.versioning.cs
@@ -52,7 +52,7 @@ internal partial class Build : NukeBuild
             Git($"config --global user.email \"carlos.angulo.mascarell@outlook.com\"", logOutput: true);
             Git($"config --global user.name \"Carlos Angulo\"", logOutput: true);
             
-            Git("remote set - url origin https://x-access-token:${{ secrets.GITHUB_TOKEN }}@github.com/$GITHUB_REPOSITORY", logOutput: true);
+            Git("remote set-url origin https://x-access-token:${{ secrets.GITHUB_TOKEN }}@github.com/$GITHUB_REPOSITORY", logOutput: true);
             Git("checkout \"${ GITHUB_REF: 11}\"", logOutput: true);
 
             Git($"status", logOutput: true);
