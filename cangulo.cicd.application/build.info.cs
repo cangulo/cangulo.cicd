8a03679e300ae4c57432ea6d6a663c5c21395fd1
Carlos Miguel Angulo Mascarell <carlos.angulo.mascarell@outlook.com>
2021-08-01 23:33:34 +0200

    feat:created info

---
+++ cangulo.cicd.application/build.info.cs
@@ -0,0 +1,14 @@
+﻿using cangulo.cicd.Abstractions.Constants;
+using Nuke.Common;
+using System.Text.Json;
+
+internal partial class Build : NukeBuild
+{
+    private Target PrintPipelineInfo => _ => _
+        .DependsOn(ParseCICDFile)
+        .Executes(() =>
+        {
+            Logger.Info($"GitRepo:\n{JsonSerializer.Serialize(GitRepository, SerializerContants.SERIALIZER_OPTIONS)}");
+            Logger.Info($"GitHubAction:\n{JsonSerializer.Serialize(GitHubActions, SerializerContants.SERIALIZER_OPTIONS)}");
+        });
+}
