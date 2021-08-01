ebd7bad9652aad156f4ddefa53fc8f36da5eae7e
Carlos Miguel Angulo Mascarell <carlos.angulo.mascarell@outlook.com>
2021-08-01 20:55:22 +0200

    [ci] new version 0.0.1 created

--- cangulo.cicd.application/build.git.cs
+++ cangulo.cicd.application/build.git.cs
@@ -23,9 +23,11 @@ internal partial class Build : NukeBuild
             
             Git($"status", logOutput: true);
 
-            
             Git($"commit -am \"[ci] new version {CICDFile.VersioningSettings.CurrentVersion} created\"", logOutput: true);
-            var outputCmd = Git($"push --force", logOutput: false);
+
+            Git($"status", logOutput: true);
+
+            var outputCmd = Git($"push --force --verbose", logOutput: false);
 
             outputCmd.ToList().ForEach(x => Logger.Info($"{x.Type} - {x.Text}"));
 
