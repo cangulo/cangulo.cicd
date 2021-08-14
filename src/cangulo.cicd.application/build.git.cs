fbdb837aa2c5149fce2a86e42e33edf2109eefff
Carlos Angulo Mascarell <carlos.angulo.mascarell@outlook.com>
2021-08-14 09:57:41 +0200

    testing GetCommitsInAMergedPR

--- src/cangulo.cicd.application/build.git.cs
+++ src/cangulo.cicd.application/build.git.cs
@@ -41,39 +41,4 @@ internal partial class Build : NukeBuild
 
             Logger.Info($"ConventionCommit:\n{JsonSerializer.Serialize(conventionalCommit, SerializerContants.SERIALIZER_OPTIONS)}");
         });
-
-    private Target GetCommitsInAMergedPR => _ => _
-        .Executes(async () =>
-        {
-            ControlFlow.NotNull(GitHubActions, "This Target can't be executed locally");
-
-            var repoOwner = GitHubActions.GitHubRepositoryOwner;
-            var repoName = GitHubActions.GitHubRepository.Replace($"{repoOwner}/", string.Empty);
-
-            // TODO: Migrate the injection of the client to an interface
-            var ghClient = new GitHubClient(new ProductHeaderValue($"{repoOwner}"));
-            ghClient.Credentials = new Credentials(GitHubToken);
-            var client = ghClient.Repository.PullRequest;
-
-            var query = new PullRequestRequest
-            {
-                State = ItemStateFilter.Closed,
-                Base = GitHubActions.GitHubBaseRef,
-                Head = GitHubActions.GitHubHeadRef,
-                SortProperty = PullRequestSort.Updated
-            };
-            var apiOptions = new ApiOptions { PageCount = 1, PageSize = 1 };
-            var prMerged = (await client.GetAllForRepository(repoOwner, repoName, query, apiOptions)).Single();
-
-            // Logger.Info($"PR:\n{JsonSerializer.Serialize(prMerged, SerializerContants.SERIALIZER_OPTIONS)}");
-
-            var commits = await client.Commits(repoOwner, repoName, prMerged.Number);
-            Logger.Info($"Commits found:{commits.Count}");
-            commits
-                .ToList()
-                .ForEach(x =>
-                {
-                    Logger.Info($"{x.Sha}: {x.Commit.Message}");
-                });
-        });
 }
