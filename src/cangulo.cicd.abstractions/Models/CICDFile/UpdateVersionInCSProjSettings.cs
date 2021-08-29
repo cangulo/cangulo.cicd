5008fba8c5d9d3b2859fae8910d841118a7ccf46
Carlos Miguel Angulo Mascarell <carlos.angulo.mascarell@outlook.com>
2021-08-29 21:18:43 +0200

    patch: implemented target to upload version in csproj

---
+++ src/cangulo.cicd.abstractions/Models/CICDFile/UpdateVersionInCSProjSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace cangulo.cicd.abstractions.Models.CICDFile
+{
+    public class UpdateVersionInCSProjSettings
+    {
+        public string ProjectPath { get; set; }
+        public string PreReleaseVersionSuffix { get; set; }
+    }
+}
